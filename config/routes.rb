671569bd840d6d4c862a2d4c572ed26f1862e922
Mario Lugo <mariolugo23@gmail.com>
2016-09-22 21:39:27 -0700

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/home', to: 'home_page#home'
   get '/help', to: 'home_page#help'
   get '/signup', to: 'users#new'
-  
+
+  resources :users
+  post '/signup',  to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
